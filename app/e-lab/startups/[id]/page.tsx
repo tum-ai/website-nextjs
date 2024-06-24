3e5fad8f402740aeb42b826b517fb00eb905aeaa
lankabelgezogen <cybertom1234@gmail.com>
2024-06-24 10:27:28 +0200

    fixed lint issues

--- app/e-lab/startups/[id]/page.tsx
+++ app/e-lab/startups/[id]/page.tsx
@@ -2,9 +2,9 @@ import startups from '@data/e-lab-startups.json';
 import StartupDetails from '@components/StartupDetails';
 
 export async function generateStaticParams() {
-    return startups.map((startup) => ({
+    return await Promise.resolve(startups.map((startup) => ({
         id: startup.id,
-    }));
+    })));
 }
 
 export default function StartupPage({ params }: { params: { id: string } }) {
