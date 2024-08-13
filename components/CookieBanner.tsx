0a36ac06e05cb7a72002d060fa2ae780f8a9635e
Cyro292 <amon@archi.de>
2024-08-14 00:34:28 +0200

    fixed bug with cookie value

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -5,23 +5,25 @@
 import Link from "next/link";
 import { getLocalStorage, setLocalStorage } from "./lib/cookieStorage";
 import { useState, useEffect } from "react";
+import { faL } from "@fortawesome/free-solid-svg-icons";
 
 export default function CookieBanner() {
-  const [cookieConsent, setCookieConsent] = useState(false);
+  const [cookieConsent, setCookieConsent] = useState(undefined);
 
   useEffect(() => {
     const storedCookieConsent = getLocalStorage("cookie_consent", null);
 
     setCookieConsent(storedCookieConsent);
   }, [setCookieConsent]);
-
+  
   useEffect(() => {
     const newValue = cookieConsent ? "granted" : "denied";
-
+    
     window.gtag("consent", "update", {
       analytics_storage: newValue,
     });
-
+    
+    if (cookieConsent === undefined) return;
     setLocalStorage("cookie_consent", cookieConsent);
   }, [cookieConsent]);
   return (
@@ -30,7 +32,7 @@ export default function CookieBanner() {
                   fixed bottom-0 left-0 right-0 
                   flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
                   bg-gray-700 rounded-lg shadow z-50
-                  ${cookieConsent !== null ? "hidden" : "flex"}`}
+                  ${(cookieConsent === undefined || cookieConsent !== null) ? "hidden" : "flex"}`}
     >
       <div className="text-cente text-white-200">
         <Link href="/info/cookies">
