0456d31b80863a6c27718a05740d352533544d8c
Sofiia Danylchenko <sofiya.dan@gmail.com>
2024-08-14 02:52:57 +0300

    fix: adjust for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from "@components/Header";
 import Footer from "@components/Footer";
 import GoogleAnalytics from "@components/GoogleAnalytics";
 import CookieBanner from "@components/CookieBanner";
+import { Suspense } from "react";
 
 export default function RootLayout({
   children,
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <GoogleAnalytics GA_MEASUREMENT_ID="G-X80DYJS6SW" />
+      <Suspense>
+        <GoogleAnalytics GA_MEASUREMENT_ID="G-X80DYJS6SW" />
+      </Suspense>
       <body>
         <Providers>
           <div className={inter.className}>
