08a3ddd954938c1fe1c38da63104abebe51d72aa
lankabelgezogen <cybertom1234@gmail.com>
2024-09-29 11:01:20 +0200

    change to original layout

--- app/e-lab/team/page.tsx
+++ app/e-lab/team/page.tsx
@@ -1,9 +1,8 @@
 import type {Metadata} from "next";
 import Section from "@ui/Section";
 import Link from "next/link";
-import {alumni, faq} from "../../../data/e-lab";
+import {alumni, faq, team} from "../../../data/e-lab";
 import FAQ from "@components/FAQ";
-import VentureTeam from "@components/VentureTeam";
 import Person from "@components/Person";
 
 export const metadata: Metadata = {
@@ -16,15 +15,32 @@ export default function Page() {
     return (
         <>
             <h1 className="hidden">Meet the Team behind the AI E-LAB | Venture Department Members</h1>
-            <VentureTeam />
+            <Section className="bg-purple-950 text-white">
+                <div className="mt-8 sm:mt-0 mb-8 md:w-3/5 lg:mb-16">
+                    <h2 className="mb-12 bg-gradient-to-r from-yellow-500 to-red-500 bg-clip-text text-5xl font-semibold uppercase text-transparent">
+                        Meet the Team behind the AI E-LAB
+                    </h2>
+                    <p className="text-2xl">
+                        <span className="text-yellow-500">
+                            The AI Entrepreneurship Lab is organized by passionate TUM.ai Members
+                        </span>{" "}
+                        who are committed to fostering the next generation of AI entrepreneurs. Our mission is to ignite
+                        your innovative spirit and equip you with the relevant know-how to build your own AI startup with the help of the Munich Entrepreneurship Ecosystem.
+                    </p>
+                </div>
+                <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+                    {team?.map((person) => (
+                        <Person person={person} key={person.id}/>
+                    ))}
+                </div>
+            </Section>
             <Section className="bg-purple-950 text-white">
                 <div className="mt-8 sm:mt-0 mb-8 md:w-3/5 lg:mb-16">
                     <h2 className="mb-12 bg-gradient-to-r from-yellow-500 to-red-500 bg-clip-text text-5xl font-semibold uppercase text-transparent">
                         Advisors & Honorable Alumni
                     </h2>
                 </div>
-                <div
-                    className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+                <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
                     {alumni?.map((person) => (
                         <Person person={person} key={person.id}/>
                     ))}
