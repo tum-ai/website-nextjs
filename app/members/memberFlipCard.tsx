1aa25455179ed63e1627d3819b6152f6e392a7fb
Sofiia Danylchenko <sofiya.dan@gmail.com>
2024-11-18 16:58:14 +0100

    fix: fix linting

--- app/members/memberFlipCard.tsx
+++ app/members/memberFlipCard.tsx
@@ -1,3 +1,5 @@
+// Source: https://github.com/mematthew123/3dFlipCards-demo
+
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
@@ -8,8 +10,6 @@ interface Step {
   gradient: string;
   description: string;
 }
-
-// Source: https://github.com/mematthew123/3dFlipCards-demo
 
 const steps: Step[] = [
   {
